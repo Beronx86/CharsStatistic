52bf270b3d753fdb4ecd41eefdcc75c19856d19f
v-penlu <laupeng1989@gmail.com>
2015-03-09 13:41:17 +0800

    Collect char frequency and average relative position

--- CharStatistic.py
+++ CharStatistic.py
@@ -2,6 +2,7 @@ __author__ = 'v-penlu'
 import xml.etree.ElementTree as ET
 from collections import Counter
 from random import shuffle
+import numpy as np
 
 
 xml_file = r"D:\newsspace200.xml"
@@ -45,33 +46,46 @@ f.close()
 
 chars = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
 char_stat = []
+char_pos = []
+news_cnt = 0
 for v in range(len(category_dic)):
     char_stat.append([])
+    char_pos.append([])
     for pair in news_list[v]:
         cnt = Counter()
+        pos = Counter()
         s = pair[0] + pair[1]
         s = s.lower()
         if s != "":
-            for c in s:
+            news_cnt += 1
+            for idx, c in enumerate(s):
                 cnt[c] += 1
-            stat = [cnt[x] for x in chars]
+                pos[c] += idx
             char_stat[v].append([cnt[x] for x in chars])
+            # average of relative position
+            char_pos[v].append([(pos[x] / (float(len(s) * cnt[x]))
+                                 if cnt[x] != 0. else 0.) for x in chars])
 
 four_categ = ["World", "Sports", "Business", "Sci/Tech"]
 
-f_train = open("train_75.arff", "w")
-f_test = open("test_75.arff", "w")
+f_train = open("train_pos.arff", "w")
+f_test = open("test_pos.arff", "w")
 print >> f_train, "@RELATION news_classify\n"
 print >> f_test, "@RELATION news_classify\n"
 for i, c in enumerate(chars):
     print >> f_train, "@ATTRIBUTE prob_%d  NUMERIC" % i
     print >> f_test, "@ATTRIBUTE prob_%d NUMERIC" % i
+for i, c in enumerate(chars):
+    print >> f_train, "@ATTRIBUTE pos_%d  NUMERIC" % i
+    print >> f_test, "@ATTRIBUTE pos_%d  NUMERIC" % i
 class_str = ",".join(four_categ)
 class_str = "{" + class_str + "}"
 print >> f_train, "@ATTRIBUTE class %s\n\n@DATA" % class_str
 print >> f_test, "@ATTRIBUTE class %s\n\n@DATA" % class_str
 
-out_format = "%.6f," * len(chars) + "%s"
+out_format = "%.6f," * len(chars) * 2 + "%s"
+inedx = 1
+# index = 0.75
 for k, v in category_dic.items():
     if k in four_categ:
         length = len(char_stat[v])
@@ -81,16 +95,18 @@ for k, v in category_dic.items():
         test_idx = idx[40000:41100]
         for sample_idx in train_idx:
             # 0.75 refers to word2vec unigram table
-            un_norm = map(lambda a: pow(float(a), 0.75), char_stat[v][sample_idx])
+            un_norm = map(lambda a: pow(float(a), inedx), char_stat[v][sample_idx])
             total = sum(un_norm)
             prob = map(lambda a: a / total, un_norm)
-            prob.append(k)
-            print >> f_train, out_format % tuple(prob)
+            feat = prob + char_pos[v][sample_idx]
+            feat.append(k)
+            print >> f_train, out_format % tuple(feat)
         for sample_idx in test_idx:
-            un_norm = map(lambda a: pow(float(a), 0.75), char_stat[v][sample_idx])
+            un_norm = map(lambda a: pow(float(a), inedx), char_stat[v][sample_idx])
             total = sum(un_norm)
             prob = map(lambda a: a / total, un_norm)
-            prob.append(k)
-            print >> f_test, out_format % tuple(prob)
+            feat = prob + char_pos[v][sample_idx]
+            feat.append(k)
+            print >> f_test, out_format % tuple(feat)
 
 
